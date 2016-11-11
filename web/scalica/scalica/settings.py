042093b564257f98986b12aaea7d0c21ab9da1bf
Yair Sovran <yys220@nyu.edu>
2016-11-10 22:17:38 -0500

    Point setting to new db setting.

--- web/scalica/scalica/settings.py
+++ web/scalica/scalica/settings.py
@@ -98,16 +98,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'scalica.wsgi.application'
 
-
-# Database
-DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'OPTIONS': {
-      'read_default_file': os.path.join(BASE_DIR, '..', 'depot', 'db', 'my.cnf'),
-    },
-  }
-}
+# Database options are read from a different file, so we can change them per
+# deployment.
+execfile(os.path.join(BASE_DIR, '..', '..', 'db', 'db_settings.py'))
 
 # Database routers go here:
 # DATABASE_ROUTERS = ['micro.routers.UserRouter']
