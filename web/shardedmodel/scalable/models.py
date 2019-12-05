0b59e1ed6fe3d6006de70f2ae3738bb5de969812
Mengyang <mengyzhang87@gmail.com>
2019-12-05 02:28:35 -0500

    Revert "Added utilities to generate uuid based on index and according tests"
    This reverts commit 4b375b7beee75d10ba949514c9cb0454e993ff23.

--- web/shardedmodel/scalable/models.py
+++ web/shardedmodel/scalable/models.py
@@ -1,51 +1,4 @@
 from django.db import models
-
-# Create your models here.
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-from django_autoshard import utils
-from django_autoshard.managers import ShardedManager, UserManager
-
-
-class ShardedModel(models.Model):
-    id = models.BigIntegerField(primary_key=True)
-
-    objects = ShardedManager()
-
-    class Meta:
-        abstract = True
-
-    def __init__(self, *args, **kwargs):
-        super(ShardedModel, self).__init__(*args, **kwargs)
-        try:
-            key = getattr(self, self.SHARD_KEY)  # kwargs[self.SHARD_KEY]
-        except AttributeError:
-            raise RuntimeError('{} does not define a SHARD_KEY'.format(self.__repr__()))
-        except KeyError:
-            raise RuntimeError('{} does not have {} set'.format(self.__repr__(), self.SHARD_KEY))
-        self.__shard = utils.get_shard(key)
-
-    @property
-    def shard(self):
-        return self.__shard
-
-    def save(self, *args, **kwargs):
-        kwargs['using'] = self.shard.alias
-        if self.pk is not None:
-            return super(ShardedModel, self).save(*args, **kwargs)  # UPDATE
-        self.id = utils.generate_uuid(self.shard.index)
-        return super(ShardedModel, self).save(*args, **kwargs)  # Set the auto-id
-
-
-class ShardRelatedModel(models.Model):
-    class Meta:
-        abstract = True
-
-    @property
-    def shard(self):
-        for field in self._meta.fields:
-            pass
         
 class Mapping(models.Model):
     min_shard = models.IntegerField()
@@ -56,4 +9,3 @@ class Mapping(models.Model):
 
     def __str__(self):
         return ("[%d, %d] %d %s %s" % (self.min_shard, self.max_shard, self.perm, self.target1, self.target2)) 
-
--- web/shardedmodel/scalable/tests.py
+++ web/shardedmodel/scalable/tests.py
@@ -1,22 +1,3 @@
 from django.test import TestCase
-from scalable import utils
 
-class utilTests(TestCase):
-    def setUp(self):
-        pass
-
-    def test_generate_uuid(self):
-        print()
-        print('Generating uuid with index 1')
-        print('*** Generated uuid:', utils.generate_uuid(1))
-        print()
-        
-    def test_get_shard_id_uuid(self):
-        print('Retreving shard index from uuid created with 1')
-        uuid = utils.generate_uuid(1)
-        print('*** Retreved index:', utils.get_shard_index_from_uuid(uuid))
-        print()
-
-    # def test_get_shard_from_index(self):
-    #     print('Getting shard with index 1')
-    #     print('Shard info:', utils.get_shard_from_index(1))
+# Create your tests here.
