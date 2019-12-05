fcf3e6d92a0add0e4e6f411674616a5e73fcca76
Mengyang <mengyzhang87@gmail.com>
2019-12-05 02:02:49 -0500

    added mapping model and created admin site

--- web/shardedmodel/scalable/models.py
+++ web/shardedmodel/scalable/models.py
@@ -46,4 +46,14 @@ class ShardRelatedModel(models.Model):
     def shard(self):
         for field in self._meta.fields:
             pass
-ß
+        
+class Mapping(models.Model):
+    min_shard = models.IntegerField()
+    max_shard = models.IntegerField()
+    perm = models.IntegerField()
+    target1 = models.CharField(max_length=10)
+    target2 = models.CharField(max_length=10, blank=True)
+
+    def __str__(self):
+        return ("[%d, %d] %d %s %s" % (self.min_shard, self.max_shard, self.perm, self.target1, self.target2)) 
+
