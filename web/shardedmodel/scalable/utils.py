eb237d6eb420ff6807a329ba7038f4b8bde40b8f
Jack Zhang <hzzyy1997@gmail.com>
2019-12-08 22:20:41 +0000

    added utilities

--- web/shardedmodel/scalable/utils.py
+++ web/shardedmodel/scalable/utils.py
@@ -1,4 +1,6 @@
 from scalable.models import Mapping
+from django.conf import settings
+import importlib
 
 class MappingDict:
     # TODO： you don't need to pass all mappings to this class; just use Mapping.objects.all() here
@@ -20,3 +22,11 @@ class MappingDict:
             if (logical >= m.min_shard and logical <= m.max_shard):
                 return m.target1
         return None
+
+def migration(shard_mapping_id):
+    split_db = Mapping.objects.get(id = 3)
+    customized_models = importlib.import_module(settings.CUSTOMIZED_MODEL_MODULE)
+    shardable_models = settings.SHARDABLE_MODELS.split(',')
+    for model in shardable_models:
+        model_to_migrate = getattr(customized_models, model)
+        model_to_migrate.objects.using(split_db).all()
