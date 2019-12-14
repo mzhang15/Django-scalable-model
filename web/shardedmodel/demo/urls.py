bf954fe93fd10052f4d974e8fee014b148e8d56a
Mengyang <mengyzhang87@gmail.com>
2019-12-14 17:50:33 -0500

    added webpage and api to display posts

--- web/shardedmodel/demo/urls.py
+++ web/shardedmodel/demo/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('users/', views.user_list),
     path('users/<slug:db>', views.user_list),
     path('users/detail/<slug:pk>', views.user_detail),
+    # path('posts/', views.post_list),
+    path('posts/<slug:fk>', views.post_list),
+    # path('posts/detail/<slug:pk>', views.post_detail)
 ]
--- web/shardedmodel/demo/views.py
+++ web/shardedmodel/demo/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
-from demo.models import User
-from demo.serializers import UserSerializer
+from demo.models import User, Post
+from demo.serializers import UserSerializer, PostSerializer
 
 @csrf_exempt
 def user_list(request, db=0):
@@ -54,3 +54,63 @@ def user_detail(request, pk):
     elif request.method == 'DELETE':
         user.delete()
         return HttpResponse(status=204)
+
+
+@csrf_exempt
+def post_list(request, fk):
+    """
+    List all posts for a user, or create a new post.
+    """
+    if request.method == 'GET':
+        # posts = Post.objects.get(shard_key=fk)
+        # serializer = PostSerializer(posts, many=True)
+        user = User(name='user1')
+        post1 = Post(shard_key=user, content='fake post 1')
+        post2 = Post(shard_key=user, content='fake post 2')
+        posts = [post1, post2]
+        serializer = PostSerializer(posts, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        print("post post....")
+        data = JSONParser().parse(request)
+        print(data)
+        serializer = PostSerializer(data=data)
+        print(serializer)
+        if serializer.is_valid():
+            print("is valid...")
+            user = User(name=data['shard_key'])
+            post = Post(shard_key=user, content=data['content'])
+            print(post)
+            # post.save()
+            # serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+# @csrf_exempt
+# def post_detail(request, pk):
+#     """
+#     Retrieve, update or delete a code snippet.
+#     """
+#     try:
+#         # user = User.objects.get(pk=pk)
+#         print("post detail: ", pk)
+#         post = Post.objects.get(shard_key=pk)
+#     except Post.DoesNotExist:
+#         return HttpResponse(status=404)
+
+#     if request.method == 'GET':
+#         serializer = PostSerializer(post)
+#         return JsonResponse(serializer.data)
+
+#     elif request.method == 'PUT':
+#         data = JSONParser().parse(request)
+#         serializer = PostSerializer(post, data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data)
+#         return JsonResponse(serializer.errors, status=400)
+
+#     elif request.method == 'DELETE':
+#         post.delete()
+#         return HttpResponse(status=204)
